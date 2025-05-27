6baf3089563a564f0bea060007630b96ccfce7c0
Amauri Bueno Dos Santos <amauri_long@tutamail.com>
2025-05-27 12:26:44 -0300

    Add files via upload

--- app/src/main/java/com/teste21/TitleCommand.java
+++ app/src/main/java/com/teste21/TitleCommand.java
@@ -1,10 +1,14 @@
 package com.teste21;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.Random;
 import java.util.UUID;
@@ -49,6 +53,40 @@ public class TitleCommand implements CommandExecutor {
         player.getScheduler().execute(plugin, () -> {
             player.setDisplayName(fullTitle + player.getName());
             player.sendMessage(ChatColor.GREEN + "Seu novo título é: " + fullTitle);
+
+            // Aplicar efeitos ou conceder itens especiais com base no título
+            switch (title) {
+                case "Guerreiro Lendário":
+                    player.getInventory().addItem(new ItemStack(Material.DIAMOND_SWORD));
+                    player.sendMessage(ChatColor.GOLD + "Você recebeu uma espada lendária!");
+                    break;
+                case "Mestre da Magia":
+                    player.getInventory().addItem(new ItemStack(Material.MAGMA_CREAM, 3));
+                    player.getInventory().addItem(new ItemStack(Material.BLAZE_POWDER, 3));
+                    player.sendMessage(ChatColor.GOLD + "Você recebeu 3 Creme de Magma e 3 Pó de Blaze!");
+                    break;
+                case "Explorador Sombrio":
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 600, 1));
+                    player.sendMessage(ChatColor.DARK_PURPLE + "Você recebeu invisibilidade por 30 segundos!");
+                    break;
+                case "Titã do PvP":
+                    player.getInventory().addItem(new ItemStack(Material.DIAMOND_CHESTPLATE));
+                    player.sendMessage(ChatColor.RED + "Você recebeu uma armadura poderosa!");
+                    break;
+                case "Caçador de Tesouros":
+                    player.getInventory().addItem(new ItemStack(Material.GOLDEN_APPLE, 2));
+                    player.sendMessage(ChatColor.YELLOW + "Você recebeu 2 Maçãs Douradas místicas!");
+                    break;
+                case "Rei da Sobrevivência":
+                    player.getInventory().addItem(new ItemStack(Material.COOKED_BEEF, 10));
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 200, 1));
+                    player.sendMessage(ChatColor.GREEN + "Você recebeu comida especial e regeneração temporária!");
+                    break;
+                case "Arquimago Misterioso":
+                    player.getInventory().addItem(new ItemStack(Material.ENCHANTED_BOOK));
+                    player.sendMessage(ChatColor.BLUE + "Você recebeu um livro de encantamentos poderosos!");
+                    break;
+            }
         }, () -> player.sendMessage(ChatColor.RED + "Erro ao definir título!"), 0);
 
         return true;
